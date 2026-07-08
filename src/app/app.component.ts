da749d5644615707389b6545c1aea59728d4c241
miguelangelzk23 <miguelangelzk0405@gmail.com>
2026-07-07 23:13:25 -0500

    ajustes PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,21 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'aqua_vida';
+  
+  private swUpdate = inject(SwUpdate);
+
+  ngOnInit() {
+    // PWA Best Practice: Check for updates and prompt user to reload
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe((event) => {
+        if (event.type === 'VERSION_READY') {
+          if (confirm('Hay una nueva versión de Aqua Vida disponible. ¿Deseas actualizar ahora?')) {
+            window.location.reload();
+          }
+        }
+      });
+    }
+  }
 }
